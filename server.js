4606cd8d00dae46a8e12224c19d9db0a7475bdf7
Gilles Margerin <gillesmargerin@gmail.com>
2021-01-14 11:55:17 +0100

    locally creating user function

--- server.js
+++ server.js
@@ -1,18 +1,58 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    retryWrites: true,
+  })
+  .then(() => console.log("connected to db"))
+  .catch((err) => console.error("err", err.stack));
+
+const { Schema } = mongoose;
+const userSchema = new Schema({
+  _id: String,
+  username: String,
+  date: {type: Date, default: Date.now},
+  duration: Number,
+  description: String
+})
+const User = mongoose.model('User', userSchema)
+
+/* function formatDate(date) {
+  return typeof date !== 'string' ? 
+   `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`  
+  : date
+} */
+
+app.use(cors());
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+app.post("/api/exercise/new-user", async (req, res) => {
 
-
-
+  const user = new User({
+    _id: new mongoose.Types.ObjectId(),
+    username: req.body.username,
+  })
+  await user.save()
+  
+  res.send({
+    useername: user.username,
+    _id: user._id
+  })
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
