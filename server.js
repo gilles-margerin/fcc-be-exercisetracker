07133af3d198b78c29303fb365754a4bf55476d5
Gilles Margerin <gillesmargerin@gmail.com>
2021-01-14 16:03:38 +0100

    user creation to db and return object

--- server.js
+++ server.js
@@ -1,28 +1,31 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose
-  .connect(process.env.DB_URI, {
+mongoose.connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     retryWrites: true,
   })
-  .then(() => console.log("connected to db"))
-  .catch((err) => console.error("err", err.stack));
+
+const connection = mongoose.connection;
+
+connection.once("open", function () {
+  console.log("MongoDB database connection established successfully");
+});
 
 const { Schema } = mongoose;
 const userSchema = new Schema({
   _id: String,
   username: String,
-  date: {type: Date, default: Date.now},
+  date: { type: Date, default: Date.now },
   duration: Number,
-  description: String
-})
-const User = mongoose.model('User', userSchema)
+  description: String,
+});
+const User = mongoose.model("User", userSchema);
 
 /* function formatDate(date) {
   return typeof date !== 'string' ? 
@@ -32,26 +35,25 @@ const User = mongoose.model('User', userSchema)
 
 app.use(cors());
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.post("/api/exercise/new-user", async (req, res) => {
-
   const user = new User({
     _id: new mongoose.Types.ObjectId(),
     username: req.body.username,
-  })
-  await user.save()
-  
+  });
+  const userCreated = await user.save();
+
   res.send({
-    useername: user.username,
-    _id: user._id
-  })
-})
+    username: userCreated.username,
+    _id: userCreated._id
+  });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
