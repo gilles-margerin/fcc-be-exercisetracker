ed1dd00004c308d23222c73adf81d3524a332b17
Gilles Margerin <gillesmargerin@gmail.com>
2021-02-08 23:57:40 +0100

    changed catch for try/catch

--- server.js
+++ server.js
@@ -40,24 +40,27 @@ app.post("/api/exercise/new-user", async(req, res) => {
   const checkUser = await User.exists({username: req.body.username})
 
   if (checkUser) {
-    res.send('User already exists')
-  } else {
-    const user = new User({
-      _id: new mongoose.Types.ObjectId(),
-      username: req.body.username,
-    });
-    const userCreated = await user.save();
-  
-    res.send({
-      username: userCreated.username,
-      _id: userCreated._id
-    });
-  }
+    res.status(409).send('User already exists') //conflict between new user and exisiting user with same name
+    return
+  } 
+
+  const user = await new User({
+    _id: new mongoose.Types.ObjectId(),
+    username: req.body.username,
+  }).save();
+
+  res.send({
+    username: user.username,
+    _id: user._id
+  });
 });
 
 
 app.get("/api/exercise/users", async(req, res) => {
-  const users = await User.find({})
+  const users = await User.find({}).catch(err => {
+    if(err) res.sendStatus(500) //if users array can't be returned failure is due to server
+    return
+  })
   res.send(users.map(user =>
     ({username: user.username, _id: user._id})
   ))
@@ -70,44 +73,59 @@ app.post("/api/exercise/add", async(req, res) => {
     date: req.body.date ? new Date(req.body.date) : new Date()
   }
 
-  const user = await User.findById(req.body.userId).catch(err => console.log(err))
+  try {
+    const user = await User.findById(req.body.userId)
+    user.log.push(exercise)
 
-  user.log.push(exercise)
+    const result = await user.save()
 
-  const result = await user.save().catch(err => console.log(err))
+    res.send({
+      _id: result._id,
+      username: result.username,
+      date: exercise.date.toDateString(),
+      duration: exercise.duration,
+      description: exercise.description
+    })
 
-  res.send({
-    _id: result._id,
-    username: result.username,
-    date: exercise.date.toDateString(),
-    duration: exercise.duration,
-    description: exercise.description
-  })
+  } catch (err) {
+    if (err instanceof TypeError) {
+      res.status(404).send('User not found') //handling unknown id
+      return
+    } else {
+      res.sendStatus(500) //server error when trying to user.save()
+    }
+  }
 })
 
 app.get("/api/exercise/log", async(req, res) => {
   const from = req.query.from ? new Date(req.query.from) : ""
   const to = req.query.to ? new Date(req.query.to) : ""
 
-  const user = await User.findById(req.query.userId).catch(err => console.log(err))
+  try {
+    const user = await User.findById(req.query.userId)
 
-  const log = user.log.filter(exercise => {
-    if (from && to) {
-      return (exercise.date >= from && exercise.date <= to)
-    } else if (from) {
-      return exercise.date >= from
-    } else if (to) {
-      return exercise.date <= to
-    }
-    return exercise
-  }).slice(0, req.query.limit);
+    const log = user.log.filter(exercise => {
+      if (from && to) {
+        return (exercise.date >= from && exercise.date <= to)
+      } else if (from) {
+        return exercise.date >= from
+      } else if (to) {
+        return exercise.date <= to
+      }
+      return exercise
+    }).slice(0, req.query.limit);
 
-  res.send({
-    _id: user._id,
-    username: user.username,
-    count: log.length,
-    log: log
-  })
+    res.send({
+      _id: user._id,
+      username: user.username,
+      count: log.length,
+      log: log
+    })
+
+  } catch (err) {
+    res.status(404).send('User not found') //handling unknown id
+    return
+  }
 });
 
 const listener = app.listen(process.env.PORT, () => {
